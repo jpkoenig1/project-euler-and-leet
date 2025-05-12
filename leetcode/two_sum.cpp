d88b67b1a8ed4053779ba3b3155090e36ad80e86
Jpkoenig <jpkoenig@proton.me>
2025-05-11 21:22:25 -0400

    Adde my leetcode solution to 'Two-Sum' for archive.

---
+++ leetcode/two_sum.cpp
@@ -0,0 +1,31 @@
+#include "../ll_framework.h"
+#include "../ll_registry.h"
+#include <iostream>
+#include <chrono>
+
+
+std::vector<int> twoSumImpl(std::vector<int>& nums, int target) {
+    std::vector<int> solution;
+    std::unordered_map<int, int > map;
+    
+    for(int i = 0; i < nums.size(); ++i) {
+        map.emplace(target - nums[i], i);
+    }
+    for (int i = 0; i < nums.size(); ++i) {
+        if(map.contains(nums[i]) && map[nums[i]] != i) {
+            solution.push_back(i);
+            solution.push_back(map[nums[i]]);
+                return solution;
+        }
+    }
+}
+
+std::vector<int> two_sum() {
+
+    std::vector<int> exampleNums = {2,9,11,13};
+    int exampleTarget = 7;
+
+    return twoSumImpl(exampleNums, exampleTarget)
+}
+
+REGISTER_PROBLEM(two_sum)
