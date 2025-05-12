fbbbb6aac48cf869977b13f0d033fdea5f1c1ab5
Jpkoenig <jpkoenig@proton.me>
2025-05-11 23:02:38 -0400

    Removed unused imports.

--- ll_framework.h
+++ ll_framework.h
@@ -2,30 +2,26 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <unordered_map> 
 #include <chrono>
 #include <any> 
 
 namespace ProblemFramework {
-    using std::vector;
-    using std::string;
-    using std::unordered_map;
     
-    
+
     // Helper function to cast outputs appropriately. Possibly need to recurse through containers.
     std::string resolveType(std::any result_any);
 
     // Run the function/problem and benchmark results.
     template<typename Func>
     void benchmark(Func&& f, const std::string& problem_name) {
-        // Fully qualify chrono components
+        // Get the timing for solution.
         auto start = std::chrono::high_resolution_clock::now();
         std::any result_any = f();
         auto end = std::chrono::high_resolution_clock::now();
         
         std::string result_str = resolveType(result_any);
 
-        // Explicitly use std::cout
+        // Print results to terminal.
         std::cout << problem_name << " result: " << result_str
                   << " | Time: "
                   << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
