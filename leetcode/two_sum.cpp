76c88d36fe6c46a5d8b4df6d0b670c7f0f880721
Jpkoenig <jpkoenig@proton.me>
2025-05-14 03:33:13 -0400

    Making my two_sum solution slightly more efficient by doing a single pass and return early. Slighlty optimizied map find()/iterator dereference.

--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -2,21 +2,24 @@
 #include "../ll_registry.h"
 
 std::vector<int> twoSumImpl(std::vector<int>& nums, int target) {
-    std::vector<int> solution;
-    std::unordered_map<int, int > map;
-    
-    for(int i = 0; i < nums.size(); ++i) {
-        map.emplace(target - nums[i], i);
-    }
-    for (int i = 0; i < nums.size(); ++i) {
-        if(map.contains(nums[i]) && map[nums[i]] != i) {
-            solution.push_back(i);
-            solution.push_back(map[nums[i]]);
+        std::vector<int> solution;
+        std::unordered_map<int, int > indexMap;
+        
+       for(int i = 0; i < nums.size(); ++i) {
+           int value = nums[i];
+           
+            auto otherIndex = indexMap.find(value);
+            if ( otherIndex != indexMap.end()) {
+                solution.push_back(i);
+                solution.push_back(otherIndex->second);
                 return solution;
+            }    
+            else{           
+                indexMap.emplace(target - nums[i], i);
+            }
         }
+        return solution;
     }
-    return solution;
-}
 
 std::vector<int> two_sum() {
 
